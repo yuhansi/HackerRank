c40eb44e4b5ceaa4e58a4135a21c3bf6002febf7
Yuhan(Hans) Si <yuhansi@usc.edu>
2016-08-27 10:30:47 -0700

    Update Day_1.cpp

--- 30 Days of Code/Day_1.cpp
+++ 30 Days of Code/Day_1.cpp
@@ -10,21 +10,21 @@ int main() {
 	string s = "HackerRank ";
 	
 	// Declare second integer, double, and String variables.
-   int j;
-   double e;
-   string s2;
-   // Read and save an integer, double, and String to your variables.
-   cin >> j;
-   cin >> e;
-   cin.ignore();
-   getline(cin, s2);
-   // Print the sum of both integer variables on a new line.
-   cout << i + j << endl;
-   // Print the sum of the double variables on a new line.
-   cout << fixed << setprecision(1) << e + d << endl;
-   // Concatenate and print the String variables on a new line
-   // The 's' variable above should be printed first.
-   cout << s + s2 << endl;
+   	int j;
+   	double e;
+   	string s2;
+   	// Read and save an integer, double, and String to your variables.
+   	cin >> j;
+   	cin >> e;
+   	cin.ignore();
+   	getline(cin, s2);
+   	// Print the sum of both integer variables on a new line.
+   	cout << i + j << endl;
+   	// Print the sum of the double variables on a new line.
+   	cout << fixed << setprecision(1) << e + d << endl;
+   	// Concatenate and print the String variables on a new line
+   	// The 's' variable above should be printed first.
+   	cout << s + s2 << endl;
    
-   return 0;
+   	return 0;
 }
